23bf2d172852c6c7d957a348398ebcff5d628b4a
Nigam <patelnigam136@gmail.com>
2023-04-06 17:06:51 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ChatBot from "react-simple-chatbot";
 
 function App() {
+  const steps = [
+    {
+      id: "greet",
+      message: "Hello welcome to chatbot",
+      trigger: "name",
+    },
+    {
+      id: "name",
+      message: "please enter your name",
+      trigger: "wait1",
+    },
+    {
+      id: "wait1",
+      user: true,
+      trigger: "name1",
+    },
+    {
+      id: "name1",
+      message: "hii {previousValue}, please enter domain... ",
+      trigger: "wait2",
+    },
+
+    {
+      id: "wait2",
+      user: true,
+      trigger: "issue",
+    },
+    {
+      id: "issue",
+      message: "enter your {previousValue} related query...",
+      trigger: "wait3",
+    },
+    {
+      id: "wait3",
+      user: true,
+      trigger: "topic",
+    },
+    {
+      id: "topic",
+      message: "thanks! for telling your issue...",
+      end: true,
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <ChatBot steps={steps} />
+      </div>
     </div>
   );
 }
